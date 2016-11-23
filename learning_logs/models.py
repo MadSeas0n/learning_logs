5cd103ff2a2c1eff31187c4a737f325c42cb9ded
MadSeas0n <p1no4et@yandex.ru>
2016-11-23 15:35:03 +0300

    1 commit

---
+++ learning_logs/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Topic(models.Model):
+    """User's theme"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+
+
+class Entry(models.Model):
+    """Information that user get about theme"""
+    topic = models.ForeignKey(Topic)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        if len(self.text) > 50:
+            return self.text[:50] + "..."
+        else:
+            return self.text
